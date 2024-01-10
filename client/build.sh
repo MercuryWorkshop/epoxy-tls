59a26fd4d2cdaadda1f314632e743dfa3b78d46e
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2024-01-09 19:46:16 -0800

    nitpick build script

--- client/build.sh
+++ client/build.sh
@@ -1,7 +1,10 @@
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s inherit_errexit
+
 rm -rf out/ || true
 mkdir out/
-cargo build --target wasm32-unknown-unknown -r
+cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen --weak-refs --no-typescript --target no-modules --out-dir out/ ../target/wasm32-unknown-unknown/release/wstcp_client.wasm
 cp -r src/web/* out/
 (cd out; python3 -m http.server)
